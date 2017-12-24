bc4f5590ec9785037c77ed6bbf4617ae6cd90913
alexandar-vysotsky <alexandar.vysotsky@gmail.com>
2017-12-24 21:10:40 +0300

    1.3. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Calculate{
+/**
+* This is my first class
+*/
+	public static void main(String[] args){
+/**
+* This is the most important method in Java
+*/
+	System.out.println("I not will write: Hello World");
+	}
+}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Alexandr Vysotskiy (alexandar.vysotsky@gmail.com)
+* @version 1.0
+* @since 23.12.2017
+*/
