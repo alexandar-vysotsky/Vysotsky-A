886f3662adb1fc87d2ee440c67e64cfd5a5e3974
alexandar-vysotsky <alexandar.vysotsky@gmail.com>
2017-12-24 20:45:32 +0300

    1.2. Подключить сборщик Maven[#4086]

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
-
+/**
+* Package for calculate task.
+*
+* @author Alexandr Vysotskiy (alexandar.vysotsky@gmail.com)
+* @version 1.0
+* @since 23.12.2017
+*/
 public class Calculate{
 	public static void main(String[] args){
 	System.out.println("I not will write: Hello World");
