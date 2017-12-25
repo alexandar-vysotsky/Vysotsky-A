3ff5a0886acbc35cd480f24d72a3e02f332fa0e6
alexandar-vysotsky <alexandar.vysotsky@gmail.com>
2017-12-25 12:10:51 +0300

    1.5. Checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,13 +1,21 @@
 package ru.job4j;
 
-public class Calculate{
+public class Calculate {
 /**
 * This is my first class
 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 /**
 * This is the most important method in Java
 */
 	System.out.println("I not will write: Hello World");
+	}
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
